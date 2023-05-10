e5475bb64db12ad1a43c63a762554bf4f7bddc1d
pachonazario <lucasghit@yahoo.com>
2023-05-10 16:47:13 +0300

    Add map and content on Contact page, no styling

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
+import Map from '../../components/map/Map';
 
 function Contact() {
   return (
-    <div>Contact
+    <div>
+      <div className="contact-page-container">
+        <div className="contact-page-content-one">
+          <h1>Contact</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam sequi odit quis necessitatibus? Ipsa sapiente laudantium architecto in dicta velit quibusdam doloribus voluptatum, laborum aut, tempora dolore reiciendis pariatur ut.</p>
+          
+        </div>
+        <Map/>
+      </div>
+
       <Navbar/>
     </div>
 
