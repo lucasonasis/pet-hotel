f94c04c9134290f17b57c5e93d911f0a26fb4813
pachonazario <lucasghit@yahoo.com>
2023-05-18 23:10:26 +0300

    API booking initial steps

--- src/pages/booking/Booking.jsx
+++ src/pages/booking/Booking.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import Blog from '../../components/blog/Blog';
 import Gallery from '../../components/gallery/Gallery';
@@ -6,17 +6,40 @@ import Footer from '../../components/footer/Footer';
 import MyButton from '../../components/button/MyButton';
 import './Booking.css';
 
+function Booking() {
+  const [bookingData, setBookingData] = useState(null);
+  const apiUrl = 'https://example.com/api/bookings'; // Replace with your API URL
 
-function Booking () {
+  const handleBooking = async () => {
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'POST', // Use the appropriate HTTP method (GET, POST, etc.)
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ /* Pass the booking data here */ }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Booking request failed.');
+      }
+
+      const data = await response.json();
+      setBookingData(data); // Update state with the response data
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div>
-      <MyButton/>
-      <Navbar/>
-      <Blog/>
-      <Gallery/>
-      <Footer/>
+      <MyButton onClick={handleBooking} />
+      <Navbar />
+      <Blog />
+      <Gallery />
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default Booking;
